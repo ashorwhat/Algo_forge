6137f389292d2f6224e0b1d896f022af1dc27634
Ayush Rathore <ayushkumarrathore20@gmail.com>
2024-12-25 14:54:27 +0530

    Add files via upload

---
+++ Task21.java
@@ -0,0 +1,15 @@
+/*1234
+  1234
+  1234
+  1234*/
+public class Task21 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for (int j=1;j<=4;j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
---
+++ Task24.java
@@ -0,0 +1,18 @@
+/*1 2 3 4
+5 6 7 8
+9 10 11 12
+13 14 15 16*/
+public class Task24 {
+    public static void main(String[] args) {
+        int k=1;
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
